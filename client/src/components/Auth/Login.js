6a8a1b578e29b4e6c2e70fec651f5b988b3cc190
Travis <travisformayor@users.noreply.github.com>
2019-06-05 00:21:02 -0700

    new login component

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,53 +1,133 @@
 import React, { useState } from 'react';
-import Error from '../Error/Error';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Button from '@material-ui/core/Button';
 import AxiosModel from '../../models/axios';
+import Error from '../Error/Error';
 
-const Login = () => {
+function FormTest() {
   // Hooks
   const [ errors, setErrors ] = useState([]);
-  const [ userData, setUserData ] = useState({
+  const [ userValues, setValues ] = useState({
     username: '',
     password: '',
+    showPassword: false,
   });
 
   const handleChange = event => {
-    setUserData({
-      ...userData, // spread operator. copy the previous values ont first
-      [event.target.name]: event.target.value
+    setValues({
+      ...userValues, // spread operator. copy the previous userValues first
+      [event.target.name]: event.target.value, // Add any new changes
     })
   };
-
+  const handleClickShowPassword = () => {
+    setValues({ ...userValues, showPassword: !userValues.showPassword });
+  };
+  
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const response = await AxiosModel.login(userData);
-      // console.log('Response: ',response);
-      setErrors([]);
-      // const currentUser = response.data.success.id;
+      const response = await AxiosModel.login(userValues);
+      console.log('Response: ',response);
+      setErrors([]); // clear old errors
       localStorage.token = response.data.token;
       // success outcome: redirect? dismiss modal?
-      // history.push(`/profile/${currentUser}`)
+      // history.push(`/profile`)
     } catch(err) {
-      // console.log(err.response);
+      console.log(err.response);
       setErrors(err.response.data.errors)
     }
   }
 
-  const { username, password } = userData;
+  // Nav button css
+  const NavButton = withStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: 5,
+      border: 0,
+      color: 'white',
+      // height: 40,
+      padding: '0px 40px',
+      // marginTop: '30px',
+      boxShadow: '0 3px 5px 5px rgba(255, 105, 135, .3)',
+    },
+    label: {
+      textTransform: 'capitalize',
+      fontSize: '1.5em',
+    },
+  })(Button);
 
-  return(
-    <section className="form">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="username" value={username} onChange={handleChange} placeholder="Username" />
-        <input type="password" name="password" value={password} onChange={handleChange} placeholder="Password" />
-        <input type="submit" value="Submit" />
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={classes.container} noValidate autoComplete="off">
+        <TextField
+          required
+          // error
+          id="outlined-username"
+          className={classes.textField}
+          label="Username"
+          onChange={handleChange}
+          value={userValues.username}
+          name="username"
+          margin="normal"
+          variant="outlined"
+          // helperText="Some important text. Error messages here"
+        />
+        <TextField
+          required
+          // error
+          id="outlined-adornment-password"
+          className={classes.textField}
+          label="Password"
+          onChange={handleChange}
+          value={userValues.password}
+          name="password"
+          type={userValues.showPassword ? 'text' : 'password'}
+          margin="normal"
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  edge="end"
+                  aria-label="Toggle password visibility"
+                  onClick={handleClickShowPassword}
+                >
+                  {userValues.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <NavButton 
+          variant="contained" className={classes.button} type="submit" label="login">
+          Login
+        </NavButton>
       </form>
       {errors.map((error, index) => (
         <Error message={error.message} key={index} />
       ))}
-    </section> 
-    )
+    </>
+  );
 }
 
-export default Login;
+export default FormTest;
