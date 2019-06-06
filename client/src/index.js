18976c6c72ba33d558879eb6a4737d020a4f9717
Travis <travisformayor@users.noreply.github.com>
2019-06-06 13:06:59 -0700

    remove service worker code

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 // import configureStore from './configureStore';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
 
 // const store = configureStore();
 
@@ -15,8 +14,3 @@ ReactDOM.render((
     </BrowserRouter>
   // </Provider>
 ), document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
