c1cf6cf05afcefba36d9c5c40abe114d2ed86c06
Travis <travisformayor@users.noreply.github.com>
2019-06-06 00:23:40 -0700

    center the profile info

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { mergeClasses } from '@material-ui/styles';
 // import Error from '../Error/Error';
 // import AxiosModel from '../../models/axios';
 
@@ -6,13 +7,13 @@ const Profile = (props) => {
   const { user: {name, username, email, signupDate }} = props // avatar, twitter
   
   return(
-    <>
+    <div style={{ textAlign: 'center' }}>
       <h1>Profile</h1>
       <p>{name}</p>
       <p>{username}</p>
       <p>{email}</p>
       <p>{signupDate}</p>
-    </>
+    </div>
   )
 }
 
