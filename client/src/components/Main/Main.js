007a08930f1b4819ae0b67a59b902e72bdce3c10
Travis <travisformayor@users.noreply.github.com>
2019-10-07 21:30:25 -0700

    replace main image with locally hosted file instead of s3

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Main = () => {
   return(
-    // <p>{card._id}</p>
     <div style={{margin: '0 auto', width: '100%', textAlign: 'center'}} >
       <h1>Marker</h1>
       <h2>React AWS S3 Gallery</h2>
       <img style={{width: '90%', maxWidth: '600px', margin: '20px'}} 
-        src="https://marker-dev-981f2859.s3-us-west-2.amazonaws.com/0b5f9bbd6d8a9b8ead750b8905fdfd11.jpg"
+        src="/img/rainbow.jpg"
         alt="Main Page Bob" />
     </div>
   )
