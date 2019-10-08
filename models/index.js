c47fce43ab7ca5287162270abc24b87b115a55a0
Travis <travisformayor@users.noreply.github.com>
2019-10-07 20:10:35 -0700

    update packages

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/marker';
 
-mongoose.connect(DB_URL, {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true})
+mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true})
   .then(() => console.log('... MongoDB connected'))
   .catch((err) => console.log(err))
 
